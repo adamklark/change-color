e1b266960e704f2c506ea3d2eb47edce4ddb49e3
John Doe <adamvd9@gmail.com>
2021-02-02 11:16:49 +0100

    bouton-plusieur_couleurs

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-console.log("bonjour");
+/* console.log("bonjour");
 
 let style= document.querySelector('.styled')
 let body= document.querySelector('body')
 
 style.addEventListener('click', function(){
     body.style.backgroundColor = 'red';
+}); */
+
+
+let color = document.querySelector('#color')
+
+color.addEventListener('change', function(e){
+    document.body.style.backgroundColor = e.currentTarget.value
 });
+
